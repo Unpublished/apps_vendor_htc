9d1c80eeb25814f781f2827e454c916452c42ef0
Unpublished <unpublished@gmx.net>
2019-08-27 12:49:32 +0200

    Whitelist permissions needed for HTC Album and Camera

--- htc-apps-vendor.mk
+++ htc-apps-vendor.mk
@@ -1,5 +1,9 @@
 PRODUCT_PACKAGES += \
     Camera \
     HMSGallery
-	
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/privapp-permissions-album.xml:system/etc/permissions/privapp-permissions-album.xml \
+    $(LOCAL_PATH)/permissions/privapp-permissions-camera.xml:system/etc/permissions/privapp-permissions-camera.xml
+
 #$(call inherit-product, vendor/htc_apps/htc-apps-vendor-blobs.mk)
